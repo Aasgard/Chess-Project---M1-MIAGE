1aac92f8eddb7fac114aba643a53bbbc3cec86a1
François-Régis Lancien <francois.regis.lancien@gmail.com>
2015-11-20 18:35:24 +0100

    Remise en ordre

--- PDL/src/json/ExtractJSON.java
+++ PDL/src/json/ExtractJSON.java
@@ -1,33 +1,5 @@
 package json;
 
-import object.Game;
-import object.Opening;
-import object.Player;
-
-
-public class ExtractJSON {
-
-	public ExtractJSON() {
-	}
-
-	public boolean isGameExiste(Game g) {
-		// TODO Verifier dans le fichier
-		return false;
-	}
-	
-	public boolean isOpeningExiste(Opening o) {
-		// TODO Verifier dans le fichier
-		return false;
-	}
-	
-	public boolean isPlayerExiste(Player p) {
-		// TODO Verifier dans le fichier
-		return false;
-	}
-}
-=======
-package json;
-
 
 import java.util.HashMap;
 import java.util.List;
--- PDL/src/json/ITreatmentJSON.java
+++ PDL/src/json/ITreatmentJSON.java
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-package json;
-
-import java.util.List;
-
-import javax.swing.text.Position;
-
-import object.*;
-
-public interface ITreatmentJSON {
-
-	public void saveAllScoreToJSON(Game g, List<Integer> scores);
-	
-	public void saveTotalScoreToJSON(Game g, int totalScore);
-	
-	public void saveAverageVariation(Game g, double variable);
-	
-	public void saveBestMoveToJSON(Position p, String FEN);
-	
-	public void saveWinRateOpening(Opening o, double rate);
-	
-	public void saveErrorToJSON(Player p, int nbError);
-	
-	
-}
-=======
 package json;
 
 import java.util.List;
@@ -46,4 +20,3 @@ public interface ITreatmentJSON {
 	
 	
 }
->>>>>>> branch 'master' of https://github.com/Aasgard/Chess-Project---M1-MIAGE.git
