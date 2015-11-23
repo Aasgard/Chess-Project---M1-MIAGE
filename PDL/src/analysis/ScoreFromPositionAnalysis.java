c912dfadadd9105c6f70ddcc923d88aff41c1068
ClementG <Clement.goachet@gmail.com>
2015-11-23 11:40:23 +0100

    Classpath changed

--- PDL/src/analysis/ScoreFromPositionAnalysis.java
+++ PDL/src/analysis/ScoreFromPositionAnalysis.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package analysis;
 
 import java.util.List;
@@ -6,8 +5,8 @@ import java.util.List;
 import json.ExtractJSON;
 import json.ITreatmentJSON;
 import json.TreatmentJSON;
+import object.FEN;
 import object.Move;
-import object.Position;
 
 public class ScoreFromPositionAnalysis {
 
@@ -31,37 +30,3 @@ public class ScoreFromPositionAnalysis {
 		}
 	}
 }
-=======
-package analysis;
-
-import java.util.List;
-
-import json.ExtractJSON;
-import json.ITreatmentJSON;
-import json.TreatmentJSON;
-import object.FEN;
-import object.Move;
-import object.Position;
-
-public class ScoreFromPositionAnalysis {
-
-	private static ITreatmentJSON treatmentJSON = new TreatmentJSON();
-	private static ExtractJSON extractJSON = new ExtractJSON();
-	private static FEN bestFEN = new FEN("");
-	
-	public static void getEvolScore(List<Move> moves){
-		fen = moves.get(0).getLog();
-		for(Move move : moves){
-			isBestMove(extractJSON.extractFENAfterMove(move));
-		}
-		treatmentJSON.saveBestFENToJSON(fen);
-	}
-	
-	private static void isBestMove(FEN fen){
-		if(fen.getScore() > bestFEN.getScore()){
-			bestFEN.setPosition(fen.getPosition());
-			bestFEN.setScore(fen.getScore());
-		}
-	}
-}
->>>>>>> branch 'master' of https://github.com/Aasgard/Chess-Project---M1-MIAGE.git
