5e95aab292e41061f8738da5886587e4c86adeb0
PaulineF <flambard.pauline@laposte.net>
2015-11-24 09:15:31 +0100

    test unitaire

---
+++ PDL/src/unitTests/TestScoreAnalysis.java
@@ -0,0 +1,14 @@
+package unitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestScoreAnalysis {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ PDL/src/unitTests/TestScoreVariationAnalysis.java
@@ -0,0 +1,14 @@
+package unitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestScoreVariationAnalysis {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
