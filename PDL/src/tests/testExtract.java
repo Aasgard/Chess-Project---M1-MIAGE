d191dcdc741b1d992d8ed4bed995b7e3efe17b0b
Lancien François-Régis <francois.regis.lancien@gmail.com>
2015-11-24 15:43:44 +0100

    azerty

--- PDL/src/tests/testExtract.java
+++ PDL/src/tests/testExtract.java
@@ -21,7 +21,6 @@ public class testExtract {
 		
 		double endTime =timer.elapsedTime();
 		
-		System.out.println("Temps d'exécution des requêtes : " + endTime + " sec.");
 		System.out.println("Temps d'exécution des requêtes : " + endTime + " sec.\n");
 		System.out.println("Temps par Game : " + endTime/alGame.size() + " sec.");
 	}
