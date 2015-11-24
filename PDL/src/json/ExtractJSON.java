32dd3d5d5581a4643379e2a077350a6dfeeac197
ClementG <Clement.goachet@gmail.com>
2015-11-24 09:30:54 +0100

    Remove the existing game in the jsonFile

--- PDL/src/json/ExtractJSON.java
+++ PDL/src/json/ExtractJSON.java
@@ -4,14 +4,15 @@ package json;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.json.*;
-import javax.json.Json;
 import javax.json.stream.JsonParser;
 
 
@@ -25,7 +26,7 @@ import object.Player;
 import object.FEN;
 
 
-public class ExtractJSON {
+public class ExtractJSON implements GlobalJSON{
 
 	public ExtractJSON() {
 	}
@@ -59,7 +60,7 @@ public class ExtractJSON {
 		return new HashMap<Integer, List<Integer>>();
 	}
 
-	public FEN extractFenAfterMove(Move m){
+	public FEN extractFENAfterMove(Move m){
 		FEN fen = new FEN("null");
 		return fen;
 	}
@@ -72,13 +73,7 @@ public class ExtractJSON {
 	 * @throws IOException 
 	 */
 	public Game getGame(int idGame) throws IOException{
-		InputStream file = new FileInputStream("D:/wamp/www/PDL Website/json/");
-
-		JsonReader jsonReader = Json.createReader(file);
-		JsonObject jsonObject = jsonReader.readObject();
-
-		jsonReader.close();
-		file.close();
+		JsonObject jsonObject = readJSONFile(GlobalJSON.GAME_FILE);
 		
 		JsonArray gameArray = jsonObject.getJsonArray("game");
 		
@@ -97,15 +92,14 @@ public class ExtractJSON {
 		return null;
 	}
 	
-	
+	/**
+	 * Read the Json Object to find the game with this id
+	 * @param idGame
+	 * @return JsonObject Game or NULL
+	 * @throws IOException
+	 */
 	public JsonObject getJsonGame(int idGame) throws IOException{
-		InputStream file = new FileInputStream("D:/wamp/www/PDL Website/json/");
-
-		JsonReader jsonReader = Json.createReader(file);
-		JsonObject jsonObject = jsonReader.readObject();
-
-		jsonReader.close();
-		file.close();
+		JsonObject jsonObject = readJSONFile(GlobalJSON.GAME_FILE);
 		
 		JsonArray gameArray = jsonObject.getJsonArray("game");
 		
@@ -119,9 +113,55 @@ public class ExtractJSON {
 		return null;
 	}
 	
-	private String readJSONFile(String objectName){
+	
+	/**
+	 * Remove a game to the Json file
+	 * @param JsonObjectGame
+	 * @throws IOException
+	 */
+	public void deleteJsonGame(JsonObject JsonObjectGame) throws IOException{
+		JsonArray jsonArrayAll = readJSONFile(GlobalJSON.GAME_FILE);
 		
-		return null;
+		jsonArrayAll.remove(JsonObjectGame);
 		
+		JsonArrayBuilder gamesBuilder = Json.createArrayBuilder();
+		JsonObjectBuilder gameBuilder = Json.createObjectBuilder();
+		
+		JsonArray gamesArray = gamesBuilder.build();
+		
+		// Write in the file
+	}
+	
+	/**
+	 * Get the Json file 
+	 * @param objectName
+	 * @return
+	 * @throws IOException
+	 */
+	private JsonArray readJSONFile(String objectName) throws IOException{
+		
+		InputStream file = new FileInputStream(GlobalJSON.PATH + objectName);
+
+		JsonReader jsonReader = Json.createReader(file);
+		JsonArray jsonArray = jsonReader.readArray();
+
+		jsonReader.close();
+		file.close();
+		
+		return jsonArray;		
+	}
+
+	/**
+	 * Write the new file 
+	 * @param jsonArray
+	 * @param objectName
+	 * @throws FileNotFoundException
+	 */
+	public void writeJSONFile(JsonArray jsonArray, String objectName) throws FileNotFoundException{
+		OutputStream os = new FileOutputStream(GlobalJSON.PATH + objectName);
+        JsonWriter jsonWriter = Json.createWriter(os);
+
+        jsonWriter.writeArray(jsonArray);
+        jsonWriter.close();
 	}
 }
