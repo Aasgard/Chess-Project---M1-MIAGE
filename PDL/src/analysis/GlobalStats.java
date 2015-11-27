715043cd4434900395c11d1a8287abb1449774b6
Chouan <lepevedic.william@gmail.com>
2015-11-27 16:17:32 +0100

    ajout de la classe global stats qui servira pour créer les json qui seront utilise pour la page d'accueil du site

---
+++ PDL/src/analysis/GlobalStats.java
@@ -0,0 +1,18 @@
+
+package analysis;
+
+public class GlobalStats {
+
+	public void getGlobalStats(){
+		String nb_players = getNbPlayers();
+		String nb_events = getNbEvents();
+	}
+	
+	public void getGlobalBestPlayers(){
+		
+	}
+	
+	public getGlobalBestVar(){
+		
+	}
+}
