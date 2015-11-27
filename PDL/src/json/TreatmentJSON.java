ebf9d98bfde7a3eabe65e1ffd553ac1232685e00
PaulineF <flambard.pauline@laposte.net>
2015-11-27 12:11:18 +0100

    opening

--- PDL/src/json/TreatmentJSON.java
+++ PDL/src/json/TreatmentJSON.java
@@ -49,35 +49,19 @@ public class TreatmentJSON implements ITreatmentJSON, GlobalJSON {
 			score.put("number_move", nbMove);
 			score.put("score", fen.getScore());
 			score.put("FEN", fen.getRawFEN());
-			
+
 			scoresJson.put(score);
 			nbMove ++;
 
 		}
-		
+
 		game.put("scores", scoresJson);
-		
+
 		this.saveInFile(game, GlobalJSON.GAME_FILE, exists);
-		
+
 	}
 
 
-	private JSONObject createGameJson(Game g) {
-		JSONObject object = new JSONObject();
-		
-		object.put("id", g.getId());
-		object.put("id_white", g.getWhitePlayer().getId());
-		object.put("id_black", g.getBlackPlayer().getId());
-		object.put("PGN", g.getPGN());
-		object.put("evol_score_move", "");
-		object.put("move_average", "");
-		object.put("date", g.getDate());
-		object.put("score_total_variation", "");
-		object.put("id_opening", g.getOpening().getId());
-		object.put("scores", new JSONArray());
-		
-		return object;
-	}
 
 
 	public void saveAverageVariation(Game g, double variable) throws IOException{
@@ -126,43 +110,43 @@ public class TreatmentJSON implements ITreatmentJSON, GlobalJSON {
 		// Save the rankingPosition
 		saveInFile(outputJSON, RANKINGPOSITION_FILE);
 	}
-	
+
 	@Override
 	public void saveWinRateOpening(Opening o, int nbWhite, int nbBlack, int exaequo) throws IOException {
 		// Get the JsonObject from the game id
-				JsonObject openingJson = extractJSON.getJsonOpening(o.getId());
-
-				JsonObjectBuilder openingBuilder = Json.createObjectBuilder();
-
-				// If the game is in the Json
-				if(openingJson != null){
-					// Recreate the game already set
-					// TODO add the "average_win_rate" to je JsonObject
-					//openingBuilder.add("", openingJson).add("average_win_rate", rate);
-
-					// Update the game
-					// gameBuilder.add("average_win_rate", rate);	
-
-				}else{
-					// Create a new game in the jsonFile			
-					openingBuilder.add("id", o.getId());
-					openingBuilder.add("name", o.getName());
-					openingBuilder.add("move", o.getMoves());
-					// TODO eregistrer nbwhite black et exaequo
-					//openingBuilder.add("average_win_rate", rate);			
-				}
-				// Create the JsonObject
-				JsonObject openingJsonObject = openingBuilder.build();
-
-				// Save the game
-				saveInFile(openingJsonObject, OPENING_FILE);
+		JSONObject openingJson = extractJSON.getJsonOpening(o.getId());
+		boolean exists = true;
+		// If the game is in the Json
+		if(openingJson == null){
+			openingJson = createOpening(o);
+			exists = false;
+		}
 		
+		JSONObject white = new JSONObject();
+		white.put("name", "White");
+		white.put("y", nbWhite);
+		white.put("color", "#F6F3EE");
+		
+		JSONObject black = new JSONObject();
+		black.put("name", " Black");
+		black.put("y", nbBlack);
+		black.put("color", "#494847");
+		
+		JSONObject deuce = new JSONObject();
+		deuce.put("name", "Deuce");
+		deuce.put("y", exaequo);
+		deuce.put("color", "#A4A3A3'");
+		
+		JSONArray rate = new JSONArray();
+		rate.put(white);
+		rate.put(black);
+		rate.put(deuce);
+		openingJson.put("data", rate);
+
+		// Save the game
+		saveInFile(openingJson, OPENING_FILE, exists);
 	}
 
-	public void saveWinRateOpening(Opening o, double rate) throws IOException{
-
-		
-	}
 
 	public void saveBestFenToJSON(String pos, FEN fen){
 		JSONArray outputJSON = new JSONArray();			
@@ -183,19 +167,19 @@ public class TreatmentJSON implements ITreatmentJSON, GlobalJSON {
 	 * 
 	 */
 	public void saveErrorToJSON(HashMap<Player,HashMap<Integer,Integer>> players){
-		
+
 		JsonArray playersJson = extractJSON.readJSONFile(OPENING_FILE);
-		
+
 		JsonArrayBuilder playersBuilder = Json.createArrayBuilder();
 		JsonObjectBuilder playerBuiler = Json.createObjectBuilder();
 		JsonArrayBuilder errorsBuilder = Json.createArrayBuilder();
 		JsonObjectBuilder errorBuilder = Json.createObjectBuilder();		
-		
+
 		for (Map.Entry<Player, HashMap<Integer, Integer>> player : players.entrySet()) {
 
 			int idPlayer = player.getKey().getId();
 			playerBuiler.add("id", idPlayer);
-			
+
 			JsonObject myPlayerObject;
 			boolean playerFinded = false;
 			for (int i = 0; i < playersJson.size() && playerFinded; i++){
@@ -206,9 +190,9 @@ public class TreatmentJSON implements ITreatmentJSON, GlobalJSON {
 					playerFinded = true;
 				}
 			}
-			
+
 			for (Map.Entry<Integer, Integer> error : errors.entrySet()) {
-				
+
 			}
 			playersBuilder.add(playerBuiler);
 		}
@@ -275,9 +259,37 @@ public class TreatmentJSON implements ITreatmentJSON, GlobalJSON {
 	@Override
 	public void saveAverageVariation(int idGame, int averageVariation) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
 
+	private JSONObject createGameJson(Game g) {
+		JSONObject object = new JSONObject();
+
+		object.put("id", g.getId());
+		object.put("id_white", g.getWhitePlayer().getId());
+		object.put("id_black", g.getBlackPlayer().getId());
+		object.put("PGN", g.getPGN());
+		object.put("evol_score_move", "");
+		object.put("move_average", "");
+		object.put("date", g.getDate());
+		object.put("score_total_variation", "");
+		object.put("id_opening", g.getOpening().getId());
+		object.put("scores", "");
+
+		return object;
+	}
+
+
+	private JSONObject createOpening(Opening o) {
+		JSONObject object = new JSONObject();
+
+		object.put("id", o.getId());
+		object.put("name_opening", o.getName() + "-" + o.getVariation());
+		object.put("nb_move", o.getNbMoves());
+		object.put("move", o.getMoves());
+		object.put("win", "");
+
+		return object;
+	}
 }
