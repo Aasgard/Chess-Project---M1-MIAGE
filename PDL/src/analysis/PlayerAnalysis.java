bbb1e7e5aef16932801be661e1c3e8b77d787fcd
PaulineF <flambard.pauline@laposte.net>
2015-12-02 11:19:33 +0100

    best player

--- PDL/src/analysis/PlayerAnalysis.java
+++ PDL/src/analysis/PlayerAnalysis.java
@@ -63,8 +63,6 @@ public class PlayerAnalysis {
 		// add player to the list
 		addPlayer(whitePlayer, whiteWinner);
 		addPlayer(blackPlayer, blackWinner);
-		System.out.println("nom white : " + whitePlayer.getName());
-		System.out.println("nom black : " + blackPlayer.getName());
 	}
 	
 	/**
