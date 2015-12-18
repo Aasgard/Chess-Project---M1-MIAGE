a1b3491f3852701c030014f71c0a15104ea72649
ClementG <Clement.goachet@gmail.com>
2015-12-18 15:33:30 +0100

    TestGlobalStats

---
+++ PDL/src/unitTests/TestGlobalStats.java
@@ -0,0 +1,24 @@
+package unitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGlobalStats {
+
+	@Test
+	public void testGetGlobalStats() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGlobalBestPlayers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGlobalBestVar() {
+		fail("Not yet implemented");
+	}
+
+}
